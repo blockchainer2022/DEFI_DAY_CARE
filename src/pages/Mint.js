dccce78035e0fa4e7f0faa3010fc5f15b6a14d1a
coders003 <94458357+coders003@users.noreply.github.com>
2021-12-07 00:23:57 +0530

    update bg

--- src/pages/Mint.js
+++ src/pages/Mint.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { MintButton } from "../components/button";
 import { Link } from "react-router-dom";
